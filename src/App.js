c8ec230f5703892772a988a1f64b0e0b0d045cf2
Amaury Gz <gzamaury@hotmail.com>
2024-05-26 22:44:07 +0000

    improvement: - Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/App.js
+++ src/App.js
@@ -30,24 +30,24 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const jsxSquares = squares.map((currSquare, i, squares) =>
+    <Square value={currSquare} onSquareClick={() => handleClick(i)} />
+  );
+
+  const jsxRowsBoard = [];
+  const numSquaresByRow = 3;
+  for (let i = 0; i < jsxSquares.length; i += numSquaresByRow) {
+    jsxRowsBoard.push(
+      <div className="board-row">
+        {jsxSquares.slice(i, i + numSquaresByRow)}
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      { jsxRowsBoard }
     </div>
   );
 }
